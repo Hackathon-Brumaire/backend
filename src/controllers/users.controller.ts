406fcfe5727988292a646cf5f283c85b8da67e5e
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-03-10 00:24:01 +0100

    reformat all codebase

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,14 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { CreateUserDto } from '@dtos/users.dto';
-import { User } from '@interfaces/users.interface';
+import { UserEntity } from '@interfaces/users.interface';
 import userService from '@services/users.service';
 
 class UsersController {
   public userService = new userService();
 
-  public getUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public getUsers = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     try {
-      const findAllUsersData: User[] = await this.userService.findAllUser();
+      const findAllUsersData: UserEntity[] =
+        await this.userService.findAllUser();
 
       res.status(200).json({ data: findAllUsersData, message: 'findAll' });
     } catch (error) {
@@ -16,10 +21,16 @@ class UsersController {
     }
   };
 
-  public getUserById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public getUserById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     try {
       const userId = Number(req.params.id);
-      const findOneUserData: User = await this.userService.findUserById(userId);
+      const findOneUserData: UserEntity = await this.userService.findUserById(
+        userId,
+      );
 
       res.status(200).json({ data: findOneUserData, message: 'findOne' });
     } catch (error) {
@@ -27,10 +38,16 @@ class UsersController {
     }
   };
 
-  public createUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public createUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     try {
       const userData: CreateUserDto = req.body;
-      const createUserData: User = await this.userService.createUser(userData);
+      const createUserData: UserEntity = await this.userService.createUser(
+        userData,
+      );
 
       res.status(201).json({ data: createUserData, message: 'created' });
     } catch (error) {
@@ -38,11 +55,18 @@ class UsersController {
     }
   };
 
-  public updateUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public updateUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     try {
       const userId = Number(req.params.id);
       const userData: CreateUserDto = req.body;
-      const updateUserData: User = await this.userService.updateUser(userId, userData);
+      const updateUserData: UserEntity = await this.userService.updateUser(
+        userId,
+        userData,
+      );
 
       res.status(200).json({ data: updateUserData, message: 'updated' });
     } catch (error) {
@@ -50,10 +74,16 @@ class UsersController {
     }
   };
 
-  public deleteUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public deleteUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     try {
       const userId = Number(req.params.id);
-      const deleteUserData: User = await this.userService.deleteUser(userId);
+      const deleteUserData: UserEntity = await this.userService.deleteUser(
+        userId,
+      );
 
       res.status(200).json({ data: deleteUserData, message: 'deleted' });
     } catch (error) {
